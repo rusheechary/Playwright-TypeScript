64dc038e6dac8c9b5bb386c9eef93613ede1267e
rusheechary <rusheechary@gmail.com>
2026-03-31 11:03:03 +0530

    testing

---
+++ basics/methods-or-functions.js
@@ -0,0 +1,37 @@
+"use strict";
+// Methods or functions
+// What is a method/function in typescript?
+//if a method or function is reusable and can be used across multiple classes,
+//  then we can create a separate class for that and 
+// then we can extend that class in the class where we want to use those methods or functions,
+//  they are called reusable methods or functions and the class which contains those reusable methods or functions
+//  is called a utility class or common utils class.
+// class Superclass(ReusableMethods_functions_CommonUtils){
+//     selectDropdownValue():void{
+//         //logic to select dropdown value
+// }
+// //mouse hover and then elements will be visible and then we can click on it
+// //so, we need a mouse hover method.
+//         moveToElement():void{
+//             //logic to move to element- Mouse Hover on the specific element ,
+//             // so that it will be visible and then we can click on it.
+//         }
+// }
+// To perform any actions, we need to create methods/functions.
+//Be it verify page title, pag url, dropdown, check boxes,radio buttons,
+// mouse hover,right click, double click, drag& drop,alerts,frames,
+// windows handling, scrolling, keyboard actions, file upload/download,
+//  wait for element to be visible/clickable/present, etc. 
+// all these actions will be performed by creating methods/functions.
+let name1 = "sai";
+let rolln1 = 101;
+console.log(name1);
+console.log(rolln1);
+let name2 = "babu";
+let rolln2 = 102;
+console.log(name2);
+console.log(rolln2);
+let name3 = "ram";
+let rolln3 = 103;
+console.log(name3);
+console.log(rolln3);
